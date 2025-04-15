56e52d20d17f37642bc2322765d1d8c02a5423be
vatsShivam <46789032+vatsShivam@users.noreply.github.com>
2025-04-15 10:41:58 +0530

    Updated the Hooks

--- my-react.js
+++ my-react.js
@@ -2,49 +2,73 @@ var MyReact = MyReact || {};
 var MyReactDOM = MyReactDOM || {};
 
 (function () {
+  let currentComponent = null;
+  let hookIndex = 0;
+
+  MyReact.useState = function (initialValue) {
+    const hooks = currentComponent.__hooks || (currentComponent.__hooks = []);
+    const stateIndex = hookIndex++;
+
+    if (hooks[stateIndex] === undefined) {
+      hooks[stateIndex] = typeof initialValue === 'function' ? initialValue() : initialValue;
+    }
+
+    const setState = (newValue) => {
+      hooks[stateIndex] = typeof newValue === 'function' ? newValue(hooks[stateIndex]) : newValue;
+      MyReactDOM.render(currentComponent, MyReactDOM.__Container__);
+    };
+
+    return [hooks[stateIndex], setState];
+  };
+
+  MyReact.useEffect = function (callback, deps) {
+    const hooks = currentComponent.__hooks || (currentComponent.__hooks = []);
+    const effectIndex = hookIndex++;
+    const oldDeps = hooks[effectIndex];
+
+    let hasChanged = true;
+
+    if (oldDeps) {
+      hasChanged = deps.some((dep, i) => dep !== oldDeps[i]);
+    }
+
+    if (hasChanged) {
+      setTimeout(callback); // mimic async lifecycle
+      hooks[effectIndex] = deps;
+    }
+  };
+
   MyReact.Component = class Component {
+    constructor() {
+      this.state = {};
+      this.__hooks = [];
+    }
+
     setState(partialState) {
       Object.assign(this.state, partialState);
+      MyReactDOM.render(this, MyReactDOM.__Container__);
+    }
 
-      MyReactDOM.render(this, MyReactDOM.__Container__,);
-    };
     componentDidMount() {}
-  
-  }
+  };
 })();
 
 (function () {
   function createElement(node) {
-    
-    var element = document.createElement(node.type);
+    if (typeof node === 'string') return document.createTextNode(node);
+
+    const element = document.createElement(node.type);
+
     if (node.className) {
       element.className = node.className;
     }
-    Object.keys(node).forEach((i) => {
-      if (i.startsWith('on')) {
-        var listener = i.slice(2);
-        listener = listener.toLowerCase();
-        element.addEventListener(listener, node[i]);
+
+    Object.keys(node).forEach((key) => {
+      if (key.startsWith('on')) {
+        const eventType = key.slice(2).toLowerCase();
+        element.addEventListener(eventType, node[key]);
       }
     });
-
-    /*
-     var event = '^[on].*';
-     if(Object.keys(node).match(event)){
-       console.log(Object.keys.node);
-     }
-     */
-    /*
-     if (node.onClick) {
-       var event=node.onClick.name;
-       var size=event.length;
-       var listener=event.slice(2, size);
-       listener= listener.toLowerCase();
-       console.log(listener);
-       element.addEventListener(listener, node.onClick)
-     }
-     */
-
 
     if (node.text) {
       element.textContent = node.text;
@@ -52,24 +76,24 @@ var MyReactDOM = MyReactDOM || {};
 
     if (node.children) {
       node.children.forEach(child => {
-        var childElement = createElement(child);
-        element.append(childElement);
+        element.appendChild(createElement(child));
       });
     }
 
     return element;
   }
 
-  MyReactDOM.render = function render(Component, Container) {
+  MyReactDOM.render = function render(ComponentInstance, Container) {
     MyReactDOM.__Container__ = Container;
-    
-    var vitualDOM = Component.render();
-    
-    var realDOM = createElement(vitualDOM);
+    currentComponent = ComponentInstance;
+    hookIndex = 0;
+
+    const vdom = ComponentInstance.render();
+    const dom = createElement(vdom);
+
     Container.innerHTML = "";
-    Container.append(realDOM);
+    Container.appendChild(dom);
 
-    Component.componentDidMount();
-  }
+    setTimeout(() => ComponentInstance.componentDidMount(), 0);
+  };
 })();
-
