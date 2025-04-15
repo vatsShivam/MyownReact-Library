82e85c766e61ad63ee5bfa36ac0138c65c97444e
vatsShivam <46789032+vatsShivam@users.noreply.github.com>
2025-04-15 10:43:10 +0530

    Update my-component.js

--- my-component.js
+++ my-component.js
@@ -1,55 +1,37 @@
-class MyComponent extends MyReact.Component {
-    constructor(props) {
-      super(props);
-      this.state = { count: 0, text: 'abc' };
-  
-    }
-  
-    componentDidMount() {
-      console.log('hjk');
-    }
-  
-    onClick = () => {
-      this.setState({
-        count: this.state.count + 1,
-      });
-    }
-  
-    onDecrease = () => {
-      this.setState({
-        count: this.state.count - 1,
-      });
-    }
-  
-    render() {
-      return {
-        type: "div",
-        children: [{
-          type: "h3",
-          text: "State Example",
-        }, {
+class MyComponent {
+  render() {
+    const [count, setCount] = MyReact.useState(0);
+    const [text, setText] = MyReact.useState('Hello');
+
+    MyReact.useEffect(() => {
+      console.log("Component mounted or count changed:", count);
+    }, [count]);
+
+    return {
+      type: "div",
+      children: [
+        { type: "h3", text: "Hook State Example" },
+        {
           type: "button",
-          text: "Increase Counter",
-          onClick: this.onClick,
-        }, {
+          text: "Increase",
+          onClick: () => setCount(c => c + 1)
+        },
+        {
           type: "button",
-          text: "Decrease Counter",
-          onClick: this.onDecrease,
-  
-        }, {
+          text: "Decrease",
+          onClick: () => setCount(c => c - 1)
+        },
+        {
           type: "span",
           className: "result",
-          onBlur: () => console.log("You're not fucking gtting it"),
-          text: `Clicked ${this.state.count} times`,
-        }],
-      };
-    }
+          text: `Clicked ${count} times`
+        }
+      ]
+    };
   }
-  
-  MyReactDOM.render(
-    new MyComponent(),
-    document.getElementById("container"),
-    
-  );
-  
-  
+}
+
+MyReactDOM.render(
+  new MyComponent(),
+  document.getElementById("container")
+);
